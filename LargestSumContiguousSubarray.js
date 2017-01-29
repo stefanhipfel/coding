87c79ba9e24f8f7ee285f33b49676f4f9826b945
Stefan Hipfel <stefan.hipfel@googlemail.com>
2017-01-29 04:07:32 +0100

    Update LargestSumContiguousSubarray.js

--- LargestSumContiguousSubarray.js
+++ LargestSumContiguousSubarray.js
@@ -1,1 +1,11 @@
 
+a = [-2,-3,4,-1,-2,1,5,2]
+
+t = a.reduce(function(a, b, i, v){
+ if(i === v.length) {
+  return Math.max(Math.max(a + b, b), a)
+}
+ return Math.max(a + b, b)
+}, 0)
+
+console.log(t)
